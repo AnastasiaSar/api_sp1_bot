e6a9d7bbf5dc642d11f1045404a0ccd1953950a7
ANASTASIIA <AnastasiaSarmonova@yandex.ru>
2021-01-12 13:19:16 +0800

    My fifth commit

--- homework.py
+++ homework.py
@@ -1,9 +1,11 @@
+import logging
 import os
 import time
 
-import logging
 import requests
 import telegram
+
+
 from dotenv import load_dotenv
 
 
@@ -28,9 +30,12 @@ APPROVED_HOMEWORK = 'У вас проверили работу "{homework_name}"
 AUTHORIZATION = f'OAuth {PRAKTIKUM_TOKEN}'
 EXCEPTION_APPEARED = 'Обнаружена ошибка: {exception}, \
                       параметры запроса {params}'
-RESPONSE_ERROR = 'Ошибка: {error} Ключи ответа: {info}'
+RESPONSE_ERROR = 'Ошибка: {error}, статус ответа: {status}'
+RESPONSE_CODE = 'Ключи ответа: {info}, статус ответа: {status}'
 MESSAGE = 'Отправка сообщения в телеграм: {message}'
 BOT_EXCEPTION = 'Бот столкнулся с ошибкой: {Exception}'
+RESPONSE_SEND = 'Ошипка отправки сообщения: {error}'
+START = 'Запуск бота'
 
 
 def parse_homework_status(homework):
@@ -55,34 +60,48 @@ def get_homework_statuses(current_timestamp):
             params=params,
             headers={'Authorization': AUTHORIZATION}
         )
-        print(response)
 
-    except requests.exceptions.ConnectionError as exception:
-        raise requests.exceptions.ConnectionError(
+    except requests.exceptions.ConnectTimeout as exception:
+        raise requests.exceptions.ConnectTimeout(
             EXCEPTION_APPEARED.format(
                 exception=exception,
                 params=params,
-                url=response.url,
-                headers=response.json()
+                url=URL_HW_STATUS,
+                headers={'Authorization': AUTHORIZATION}
             )
         )
 
     status_data = response.json()
-    if 'error' in status_data and 'code' in status_data:
-        raise TypeError(RESPONSE_ERROR.format(error=status_data['error'],
-                                              info=status_data['code']))
+    if 'error' in status_data:
+        raise RuntimeError(RESPONSE_ERROR.format(
+            error=status_data['error'],
+            status=response.status_code
+            )
+        )
+    if 'code' in status_data:
+        raise RuntimeError(RESPONSE_CODE.format(
+            info=status_data['code'],
+            status=response.status_code
+            )
+        )
     return status_data
 
 
 def send_message(message, bot_client):
-    logger.info(MESSAGE.format(message=message))
-    return bot_client.send_message(chat_id=CHAT_ID, text=message)
+    try:
+        logger.info(MESSAGE.format(message=message))
+        return bot_client.send_message(chat_id=CHAT_ID, text=message)
+    except telegram.error.BadRequest:
+        raise telegram.error.BadRequest(RESPONSE_SEND.format(
+            error=telegram.error.BadRequest
+            )
+        )
 
 
 def main():
-    logger.debug('Запуск бота')
+    logger.debug(START)
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time()) - 3600 * 24
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -99,7 +118,7 @@ def main():
         except Exception:
             logger.error(BOT_EXCEPTION.format(Exception=Exception))
             send_message(
-                (BOT_EXCEPTION.format(Exception=Exception)),
+                BOT_EXCEPTION.format(Exception=Exception),
                 bot_client
             )
             time.sleep(5)
