55145da57d60e6da36227af114053584e42cec66
ANASTASIIA <AnastasiaSarmonova@yandex.ru>
2021-01-13 13:36:03 +0800

    My eighth commit

--- homework.py
+++ homework.py
@@ -10,8 +10,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger('homework')
 logger.setLevel(logging.DEBUG)
+
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -24,20 +25,22 @@ STATUSES = {
 }
 INCORRECT_STATUS = 'Некорректный статус: {status}'
 APPROVED_HOMEWORK = 'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-AUTHORIZATION = f'OAuth {PRAKTIKUM_TOKEN}'
-EXCEPTION_APPEARED = (
-    'Обнаружена ошибка: {exception}, параметры запроса {params}'
+RESPONCE_OPTIONS = (
+    'параметры запроса {params}, запрос по урлу: {url}, заголовок: {headers}'
 )
-RESPONSE_ERROR = 'Ошибка: {error}, статус ответа: {status}'
-RESPONSE_CODE = 'Ключи ответа: {info}, статус ответа: {status}'
+EXCEPTION_APPEARED = 'Обнаружена ошибка: {exception}, '+RESPONCE_OPTIONS
+RESPONSES = {
+    'error': 'Ошибка: {error}, статус ответа: {status}, '+RESPONCE_OPTIONS,
+    'code': 'Ключи ответа: {info}, статус ответа: {status}, '+RESPONCE_OPTIONS
+}
 MESSAGE = 'Отправка сообщения в телеграм: {message}'
 BOT_EXCEPTION = 'Бот столкнулся с ошибкой: {exception}'
 RESPONSE_SEND = 'Ошибка отправки сообщения: {error}'
 START = 'Запуск бота'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def parse_homework_status(homework):
-    print(homework)
     name = homework['homework_name']
     status = homework['status']
     if status not in STATUSES:
@@ -53,46 +56,35 @@ def parse_homework_status(homework):
 
 def get_homework_statuses(current_timestamp):
     params = {'from_date': current_timestamp}
+    rq_pars = {'url': URL_HW_STATUS, 'params': params, 'headers': HEADERS}
     try:
-        response = requests.get(
-            URL_HW_STATUS,
-            params=params,
-            headers={'Authorization': AUTHORIZATION}
-        )
-
+        response = requests.get(**rq_pars)
     except requests.exceptions.ConnectionError as exception:
-        raise requests.exceptions.ConnectTimeout(
+        raise ConnectionError(
             EXCEPTION_APPEARED.format(
                 exception=exception,
-                params=params,
-                url=URL_HW_STATUS,
-                headers={'Authorization': AUTHORIZATION}
+                **rq_pars
             )
         )
 
-    status_data = response.json()
-    if 'error' in status_data:
-        raise RuntimeError(RESPONSE_ERROR.format(
-            error=status_data['error'],
-            status=response.status_code
-            )
-        )
-    if 'code' in status_data:
-        raise RuntimeError(RESPONSE_CODE.format(
-            info=status_data['code'],
-            status=response.status_code
-            )
-        )
-    return status_data
+    homework_statuses = response.json()
+    for response in RESPONSES:
+        if response in homework_statuses:
+            raise RuntimeError(RESPONSES[response].format(
+                response=homework_statuses[response],
+                status=response.status_code,
+                **rq_pars
+            ))
+    return homework_statuses
 
 
 def send_message(message, bot_client):
     try:
         logger.info(MESSAGE.format(message=message))
         return bot_client.send_message(chat_id=CHAT_ID, text=message)
-    except telegram.error.BadRequest:
-        raise telegram.error.BadRequest(RESPONSE_SEND.format(
-            error=telegram.error.BadRequest
+    except TimeoutError:
+        raise TimeoutError(RESPONSE_SEND.format(
+            error=TimeoutError
             )
         )
 
@@ -100,7 +92,7 @@ def send_message(message, bot_client):
 def main():
     logger.debug(START)
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()) - 3600 * 24
+    current_timestamp = int(time.time())
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -114,18 +106,14 @@ def main():
                 current_timestamp
             )
             time.sleep(1200)
-        except Exception:
-            logger.error(BOT_EXCEPTION.format(Exception=Exception))
-            send_message(
-                BOT_EXCEPTION.format(Exception=Exception),
-                bot_client
-            )
+        except Exception as all_exceptions:
+            logger.error(BOT_EXCEPTION.format(exception=all_exceptions))
             time.sleep(5)
 
 
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        filename='homework.log'
+        filename=__file__.replace('.py', '.log')
     )
     main()
