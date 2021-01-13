fa746abea70ca6ae46249e721b149ff1f8d774c0
ANASTASIIA <AnastasiaSarmonova@yandex.ru>
2021-01-14 02:59:42 +0800

    My tenth commit

--- homework.py
+++ homework.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-logger = logging.getLogger('homework')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
@@ -23,15 +23,17 @@ STATUSES = {
     'reviewing': 'Работа "{homework}" взята в ревью',
     'rejected': 'К сожалению в работе нашлись ошибки.'
 }
-INCORRECT_STATUS = 'Некорректный статус: {status}'
+UNEXPECTED_STATUS = 'Неожиданный статус: {status}'
 APPROVED_HOMEWORK = 'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 RESPONCE_OPTIONS = (
     'параметры запроса {params}, запрос по урлу: {url}, заголовок: {headers}'
 )
-EXCEPTION_APPEARED = 'Обнаружена ошибка: {exception}, '+RESPONCE_OPTIONS
+EXCEPTION_APPEARED = (
+    'Обнаружена ошибка соединения: {exception},' + RESPONCE_OPTIONS
+)
 RESPONSES = {
-    'error': 'Ошибка: {error}, статус ответа: {status}, '+RESPONCE_OPTIONS,
-    'code': 'Ключи ответа: {info}, статус ответа: {status}, '+RESPONCE_OPTIONS
+    'error': 'Ошибка: {error}, статус ответа: {status}, ' + RESPONCE_OPTIONS,
+    'code': 'Ключи ответа: {code}, статус ответа: {status},' + RESPONCE_OPTIONS
 }
 MESSAGE = 'Отправка сообщения в телеграм: {message}'
 BOT_EXCEPTION = 'Бот столкнулся с ошибкой: {exception}'
@@ -44,7 +46,7 @@ def parse_homework_status(homework):
     name = homework['homework_name']
     status = homework['status']
     if status not in STATUSES:
-        raise ValueError(INCORRECT_STATUS.format(status=status))
+        raise ValueError(UNEXPECTED_STATUS.format(status=status))
     verdict = STATUSES[status].format(homework_name=name)
     if status == 'reviewing':
         return verdict
@@ -55,38 +57,34 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    params = {'from_date': current_timestamp}
-    rq_pars = {'url': URL_HW_STATUS, 'params': params, 'headers': HEADERS}
+    request_params = {
+        'url': URL_HW_STATUS,
+        'params': {'from_date': current_timestamp},
+        'headers': HEADERS
+    }
     try:
-        response = requests.get(**rq_pars)
+        response = requests.get(**request_params)
     except requests.exceptions.ConnectionError as exception:
         raise ConnectionError(
             EXCEPTION_APPEARED.format(
                 exception=exception,
-                **rq_pars
+                **request_params
             )
         )
-
-    homework_statuses = response.json()
-    for response in RESPONSES:
-        if response in homework_statuses:
-            raise RuntimeError(RESPONSES[response].format(
-                response=homework_statuses[response],
+    response_json = response.json()
+    for response_name, response_value in RESPONSES.items():
+        if response_name in response_json:
+            raise RuntimeError(response_value.format(
+                response_name=response_value,
                 status=response.status_code,
-                **rq_pars
+                **request_params
             ))
-    return homework_statuses
+    return response_json
 
 
 def send_message(message, bot_client):
-    try:
-        logger.info(MESSAGE.format(message=message))
-        return bot_client.send_message(chat_id=CHAT_ID, text=message)
-    except TimeoutError:
-        raise TimeoutError(RESPONSE_SEND.format(
-            error=TimeoutError
-            )
-        )
+    logger.info(MESSAGE.format(message=message))
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -95,17 +93,16 @@ def main():
     current_timestamp = int(time.time())
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get(
-                    'homeworks')[0]),
-                    bot_client
-                )
-            current_timestamp = new_homework.get(
+            response = get_homework_statuses(current_timestamp)
+            homeworks = response.get('homeworks')
+            if len(homeworks) > 0:
+                send_message(parse_homework_status(homeworks[0]),
+                             bot_client)
+            current_timestamp = response.get(
                 'current_date',
                 current_timestamp
             )
-            time.sleep(1200)
+            time.sleep(10)
         except Exception as all_exceptions:
             logger.error(BOT_EXCEPTION.format(exception=all_exceptions))
             time.sleep(5)
